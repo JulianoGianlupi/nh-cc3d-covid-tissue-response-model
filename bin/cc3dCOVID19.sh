1e6fd9fc5522402fee21ac4f6a34e6c9dfd59189
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2020-04-20 11:16:12 -0400

    Update cc3dCOVID19.sh

--- bin/cc3dCOVID19.sh
+++ bin/cc3dCOVID19.sh
@@ -3,6 +3,7 @@
 
 current_directory=$(pwd)
 
+PARENT_DIR="$(dirname "$(pwd)")"
 
 # necessary to enforce standard convention for numeric values specification on non-English OS
 export LC_NUMERIC="C.UTF-8"
@@ -11,7 +12,8 @@ CC3D_DEMO_NAME=Coronavirus
 CC3D_DEMO_FILENAME=${CC3D_DEMO_NAME}.cc3d
 USER_DEMO_PATH=$HOME/CompuCell3D_Demos/${CC3D_DEMO_NAME}
 
-MAIN_CC3D_FILES=${current_directory}:/../main/
+#MAIN_CC3D_FILES=${current_directory}:/../main/
+MAIN_CC3D_FILES=${PARENT_DIR}/main/
 
 
 mkdir -p ${USER_DEMO_PATH}
