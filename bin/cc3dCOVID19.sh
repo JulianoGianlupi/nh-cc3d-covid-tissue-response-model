f936160efe24c5f9877467d14459dc50fc104ea9
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2020-04-20 11:49:03 -0400

    Update cc3dCOVID19.sh

--- bin/cc3dCOVID19.sh
+++ bin/cc3dCOVID19.sh
@@ -18,14 +18,14 @@ USER_DEMO_PATH=$HOME/CompuCell3D_Demos/${CC3D_DEMO_NAME}
 echo ${USER_DEMO_PATH}
 
 #MAIN_CC3D_FILES=${current_directory}:/../main/
-#MAIN_CC3D_FILES=${PARENT_DIR}/main/
-MAIN_CC3D_FILES=${PARENT_DIR}main/
+MAIN_CC3D_FILES=${PARENT_DIR}/main/
+#MAIN_CC3D_FILES=${PARENT_DIR}main/
 
 echo ${MAIN_CC3D_FILES}
 
 mkdir -p ${USER_DEMO_PATH}
 mkdir -p ${USER_DEMO_PATH}/output/
 
-cp -rf ${MAIN_CC3D_FILES}/Demos/.  ${USER_DEMO_PATH} #not checking for existance incase there's been an update
+cp -rf ${MAIN_CC3D_FILES}.  ${USER_DEMO_PATH} #not checking for existance incase there's been an update
 
 ${COMPUCELL3D_HOME}/compucell3d.sh -i ${USER_DEMO_PATH}/${CC3D_DEMO_FILENAME} -o ${USER_DEMO_PATH}/output/ -f 10001 > /dev/null 2>&1 
