e2faf1e70453cd5026e32a6c333fa51026d644be
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2020-05-15 08:50:02 -0400

    putting out back to dev null

--- bin/cc3dCOVID19.sh
+++ bin/cc3dCOVID19.sh
@@ -39,5 +39,4 @@ mkdir -p ${USER_DEMO_PATH}/output/
 
 cp -rf ${MAIN_CC3D_FILES}  ${USER_DEMO_PATH} #not checking for existance incase there's been an update
 
-${COMPUCELL3D_HOME}/compucell3d.sh -i ${USER_DEMO_PATH}/${CC3D_DEMO_FILENAME} -o ${USER_DEMO_PATH}/output/ -f 10001
-#> /dev/null 2>&1 
+${COMPUCELL3D_HOME}/compucell3d.sh -i ${USER_DEMO_PATH}/${CC3D_DEMO_FILENAME} -o ${USER_DEMO_PATH}/output/ -f 10001 > /dev/null 2>&1 
